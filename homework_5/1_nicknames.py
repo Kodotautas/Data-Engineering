3598cd08a04488f8234b46279255b5daa99e3ec5
Kodotautas <lukosiunas.v@gmail.com>
2022-05-09 07:39:03 +0300

    finished app 1

--- homework_5/1_nicknames.py
+++ homework_5/1_nicknames.py
@@ -1,16 +1,11 @@
 import os
-import random 
 import pandas as pd
 import sqlite3
 
-
-# Task 5
-# 1. Take your OLTP and OLAP from HW#4 and ml-latest-small.zip dataset.
-# 2. Develop an application which generates nicknames for users in ratings.csv and loads those into a separate table in OLTP.
-# 4. Develop an app, which will prompt a user on their ID and suggest to change their nickname.
-# 5. Develop an app, which will extract only lately changed users and load those to OLAP. Chose Slowly Changing Dimension type and be ready to explain your choice. 
-# 6. Similarly to HW#2, build a view of top-3 actively voting users per each genre for a given year, but this time with nicknames. 
-# 7. Make sure that user updates from #3 are reflected in the results of #5
+# ---------------------------------- SUMMARY --------------------------------- #
+# App take  OLTP and OLAP from HW#4(ratings.csv was uploaded)
+# Develop an application which generates nicknames for users from ratings tbale
+# Uploads those into a separate table in OLTP.
 
 cwd = os.getcwd()
 
