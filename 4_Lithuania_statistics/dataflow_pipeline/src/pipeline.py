601a6e6cece7c39c7b79f8aad1d4a5420ca409d6
Kodotautas <lukosiunas.v@gmail.com>
2023-08-01 08:12:54 +0300

    fixed pipeline, no errors

--- 4_Lithuania_statistics/dataflow_pipeline/src/pipeline.py
+++ 4_Lithuania_statistics/dataflow_pipeline/src/pipeline.py
@@ -9,7 +9,7 @@ import requests
 
 
 # --------------------------------- FUNCTIONS -------------------------------- #
-def download_data(element, url):
+def download_data(url):
     """A function that downloads data from a specified URL."""
     logging.info(f'Downloading data from {url}')
     response = requests.get(url)
@@ -28,7 +28,7 @@ def parse_data(element):
         period = obs.find('g:ObsKey/g:Value[@id="LAIKOTARPIS"]', namespaces).attrib['value']
         population = obs.find('g:ObsValue', namespaces).attrib['value']
 
-        # Transform the period from "2008M12" to "2008-12"
+        # Transform format from "2008M12" to "2008-12"
         period = re.sub(r'M(\d+)$', r'-\1', period)
 
         rows.append({'period': period, 'population': population})
@@ -36,7 +36,7 @@ def parse_data(element):
 
 def save_data_to_gcs(element, bucket_name, filename):
     """A function that saves data to a Google Cloud Storage bucket as a CSV file."""
-    df = pd.DataFrame(element)
+    df = pd.DataFrame.from_records(element)
     logging.info(f'Saving data to gs://{bucket_name}/{filename}')
     bucket = storage.Client().bucket(bucket_name)
     blob = bucket.blob(filename)
@@ -51,17 +51,16 @@ def run():
     options = PipelineOptions()
     options.view_as(StandardOptions).runner = 'DataflowRunner'
     options.view_as(GoogleCloudOptions).project = 'vl-data-learn'
-    options.view_as(GoogleCloudOptions).region = 'us-central1'
+    options.view_as(GoogleCloudOptions).region = 'europe-west1'
     options.view_as(GoogleCloudOptions).job_name = 'lithuania-statistics-population'
     options.view_as(GoogleCloudOptions).staging_location = 'gs://lithuania_statistics/staging'
     options.view_as(GoogleCloudOptions).temp_location = 'gs://lithuania_statistics/temp'    
 
     with beam.Pipeline(options=options) as pipeline:
         (pipeline
-            | 'Create' >> beam.Create([None])
-            | 'Download Data' >> beam.ParDo(download_data, 'https://osp-rs.stat.gov.lt/rest_xml/data/S3R168_M3010101_1')
-            | 'Parse Data' >> beam.Map(parse_data)
-        #     | 'Save to GCS' >> beam.ParDo(save_data_to_gcs, 'lithuania_statistics', 'lithuania_monthly_population.csv')
+            | 'Download data' >> beam.Create(['https://osp-rs.stat.gov.lt/rest_xml/data/S3R168_M3010101_1'])
+            | 'Parse data' >> beam.Map(download_data)
+            | 'Save data to GCS' >> beam.Map(save_data_to_gcs, 'lithuania_statistics', 'population.csv')
         )
 
 if __name__ == '__main__':
