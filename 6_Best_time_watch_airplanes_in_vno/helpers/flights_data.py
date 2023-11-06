3bfba821d5ead7045e97717828f22d913301fb21
Kodotautas <lukosiunas.v@gmail.com>
2023-11-06 17:01:40 +0200

    saving

--- 6_Best_time_watch_airplanes_in_vno/helpers/flights_data.py
+++ 6_Best_time_watch_airplanes_in_vno/helpers/flights_data.py
@@ -1,5 +1,6 @@
 from FlightRadar24 import FlightRadar24API
 import pandas as pd
+import time
 
 
 class FlightData:
@@ -20,9 +21,9 @@ class FlightData:
         arrivals_df = pd.json_normalize(self.arrivals)
         arrivals_df['arrival_time'] = pd.to_datetime(arrivals_df['flight.time.scheduled.arrival'] + arrivals_df['flight.airport.destination.timezone.offset'], unit='s')
         arrivals_df['arrival_estimated_time'] = pd.to_datetime(arrivals_df['flight.time.estimated.arrival'] + arrivals_df['flight.airport.destination.timezone.offset'], unit='s')
-        arrivals_df['arrival_final_time'] = arrivals_df['arrival_estimated_time'].fillna(arrivals_df['arrival_time'])
+        arrivals_df['final_time'] = arrivals_df['arrival_estimated_time'].fillna(arrivals_df['arrival_time'])
         # columns to leave
-        columns_to_leave = ['flight.identification.number.default', 'flight.identification.callsign', 'flight.aircraft.model.text', 'flight.aircraft.country.name', 'flight.airline.short', 'flight.airport.origin.position.region.city', 'arrival_final_time']
+        columns_to_leave = ['flight.status.generic.status.type', 'flight.identification.number.default', 'flight.identification.callsign', 'flight.aircraft.model.text', 'flight.aircraft.country.name', 'flight.airline.short', 'flight.airport.origin.position.region.city', 'final_time']
         arrivals_df = arrivals_df[columns_to_leave]
         return arrivals_df
 
@@ -30,16 +31,26 @@ class FlightData:
         departures_df = pd.json_normalize(self.departures)
         departures_df['departure_time'] = pd.to_datetime(departures_df['flight.time.scheduled.departure'] + departures_df['flight.airport.origin.timezone.offset'], unit='s')
         departures_df['departure_estimated_time'] = pd.to_datetime(departures_df['flight.time.estimated.departure'] + departures_df['flight.airport.origin.timezone.offset'], unit='s')
-        departures_df['departure_final_time'] = departures_df['departure_estimated_time'].fillna(departures_df['departure_time'])
+        departures_df['final_time'] = departures_df['departure_estimated_time'].fillna(departures_df['departure_time'])
         # columns to leave
-        columns_to_leave = ['flight.identification.number.default', 'flight.identification.callsign', 'flight.aircraft.model.text', 'flight.aircraft.country.name', 'flight.airline.short', 'flight.airport.destination.position.region.city', 'departure_final_time']
+        columns_to_leave = ['flight.status.generic.status.type', 'flight.identification.number.default', 'flight.identification.callsign', 'flight.aircraft.model.text', 'flight.aircraft.country.name', 'flight.airline.short', 'flight.airport.destination.position.region.city', 'final_time']
         departures_df = departures_df[columns_to_leave]
         return departures_df
     
+    def concat_arrivals_departures(self):
+        arrivals_df = self.get_arrivals()
+        departures_df = self.get_departures()
+        flights_df = pd.concat([arrivals_df, departures_df])
+        flights_df = flights_df.sort_values(by='final_time', ascending=True)
+        return flights_df
 
 if __name__ == '__main__':
+    start_time = time.time()
+
     airport_code = 'EYVI'
     fd = FlightData(airport_code)
-    departures_df = fd.get_departures()
-    departures_df.to_csv('flights_data.csv', index=False)
-    print(departures_df.head(10))
+    flights = fd.concat_arrivals_departures()
+    print(f'Flights in {airport_code} airport: {len(flights)}')
+    flights.to_csv(f'{airport_code}_flights.csv', index=False)
+
+    print(f'--- {time.time() - start_time} seconds ---')
