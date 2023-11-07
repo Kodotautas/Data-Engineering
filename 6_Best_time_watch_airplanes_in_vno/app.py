0e24d9abf0762079e5e75eb5c4769961b7cf2398
Kodotautas <lukosiunas.v@gmail.com>
2023-11-07 23:57:28 +0200

    various improvements to app

--- 6_Best_time_watch_airplanes_in_vno/app.py
+++ 6_Best_time_watch_airplanes_in_vno/app.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.offline import plot
-import numpy as np
+import pandas as pd
 
 from flask import Flask, render_template
 from helpers.flights_data import FlightData
@@ -13,35 +13,31 @@ app = Flask(__name__)
 flights = FlightData('EYVI').group_flights_by_final_time()
 
 @app.route('/')
-def flights_table():
-    # Create a color scale
-    color_scale = cl.scales['9']['seq']['Blues']
-
-    # Normalize the 'Count' values to the range of the color scale
-    normalized_counts = np.interp(flights['count'], (min(flights['count']), max(flights['count'])), (0, len(color_scale) - 1))
-
-    # Map the normalized 'Count' values to the color scale
-    colors = [color_scale[int(i)] for i in normalized_counts]
-
+def flights_bar_chart():
     # Create a table of flight destinations
-    fig = go.Figure(data=[go.Table(
-        header=dict(values=['Datetime', 'Planes count']),
-        cells=dict(values=[flights['final_time'], flights['count']],
-                   fill_color=[['white'], colors])
-    )])
+    fig = px.bar(flights.sort_values('Datetime', ascending=False), 
+                 x='Flights count', y='Datetime',
+                color='Flights count',
+                orientation='h',
+                color_continuous_scale=["darkgrey", "green"],
+                )
 
     # Calculate the height of the table based on the number of rows
-    row_height = 30  # Set the height of each row in pixels
-    num_rows = len(flights['final_time'])
+    row_height = 20  # Set the height of each row in pixels
+    num_rows = len(flights['Datetime'])
     table_height = row_height * num_rows
 
     # Set the height of the table and disable scrolling
-    fig.update_layout(height=table_height, margin=dict(l=30, r=30, t=30, b=30))
-    fig.update_yaxes(visible=False, showticklabels=False)
+    fig.update_layout(height=table_height, margin=dict(l=0, r=0, t=5, b=0), template='plotly_dark')
+    fig.update_xaxes(visible=False, showticklabels=False)
+    fig.update_yaxes(visible=True, showticklabels=True, tickmode='array', tickvals=flights['Datetime'], 
+                     ticktext=flights['Datetime'].dt.strftime('%m-%d %H:%M'), 
+                     type='category'
+                     )
 
     graph = plot(fig, output_type='div')
 
-    return render_template('flight_info.html', flights=flights, graph=graph)
+    return render_template('best_time.html', flights=flights, graph=graph)
 
 if __name__ == '__main__':
     app.run(debug=True)
