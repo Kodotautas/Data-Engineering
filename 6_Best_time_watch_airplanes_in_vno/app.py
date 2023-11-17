7c1de85a5bff3d2923fc7b7e087b645c7f2f6821
Kodotautas <lukosiunas.v@gmail.com>
2023-11-17 07:27:44 +0200

    saving

--- 6_Best_time_watch_airplanes_in_vno/app.py
+++ 6_Best_time_watch_airplanes_in_vno/app.py
@@ -44,3 +44,53 @@ def flights_data():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+# import pandas as pd
+
+# from flask import Flask, render_template
+# from helpers.flights_data import FlightData
+# import datetime
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def best_time():
+#     # Get flights and weather data and filter by today and tomorrow
+#     flights = FlightData('EYVI').group_flights_by_final_time()
+#     flights = flights[(flights['Datetime'].dt.date == pd.Timestamp.today().date()) | (flights['Datetime'].dt.date == pd.Timestamp.today().date() + pd.Timedelta(days=1))]
+#     weather_data = FlightData('EYVI').get_weather()
+
+#     # Prepare data for Chart.js
+#     labels = flights.sort_values('Datetime', ascending=True)['Datetime'].tolist()
+#     labels = [datetime.datetime.strftime(x, '%Y-%m-%d %H:%M') for x in labels]
+#     data = flights.sort_values('Datetime', ascending=True)['Flights count'].tolist()
+
+#     # Pass data to template
+#     return render_template('best_time.html', labels=labels, data=data, weather_data=weather_data)
+
+
+# @app.route('/vno_flights_data')
+# def flights_data():
+#     # Get flights and weather data and filter by today and tomorrow
+#     flights_df = FlightData('EYVI').concat_arrivals_departures()
+#     weather_data = FlightData('EYVI').get_weather()
+
+#     # rename columns
+#     flights_df = flights_df.rename(columns={'flight.status.generic.status.type': 'Type', 
+#                                             'flight.identification.number.default': 'Flight number',
+#                                             'flight.aircraft.model.text': 'Aircraft model', 
+#                                             'flight.aircraft.country.name': 'Aircraft country', 
+#                                             'flight.airline.short': 'Airline', 
+#                                             'flight.airport.origin.position.region.city': 'Origin city', 
+#                                             'flight.airport.destination.position.region.city': 'Destination city', 
+#                                             'final_time': 'Final time'})
+
+#     flights_data = flights_df.to_dict('records')
+
+#     return render_template('flights_data.html', weather_data=weather_data, flights_data=flights_data, index=False)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- 6_Best_time_watch_airplanes_in_vno/helpers/flights_data.py
+++ 6_Best_time_watch_airplanes_in_vno/helpers/flights_data.py
@@ -6,40 +6,39 @@ import time
 class FlightData:
     def __init__(self, airport_code):
         self.fr_api = FlightRadar24API()
-        self.airport_details = self.fr_api.get_airport_details(airport_code)
-        self.humidity = self.airport_details['airport']['pluginData']['weather']['humidity']
-        self.sky_condition = self.airport_details['airport']['pluginData']['weather']['sky']['condition']
-        self.wind_direction = self.airport_details['airport']['pluginData']['weather']['wind']['direction']
-        self.wind_speed = self.airport_details['airport']['pluginData']['weather']['wind']['speed']['kmh']
-        self.temperature = self.airport_details['airport']['pluginData']['weather']['temp']['celsius']
-        self.arrivals = self.airport_details['airport']['pluginData']['schedule']['arrivals']['data']
-        self.departures = self.airport_details['airport']['pluginData']['schedule']['departures']['data']
-        
+        airport_details = self.fr_api.get_airport_details(airport_code)
+        airport = airport_details['airport']
+        plugin_data = airport['pluginData']
+        weather = plugin_data['weather']
+        schedule = plugin_data['schedule']
+
+        self.humidity = weather['humidity']
+        self.sky_condition = weather['sky']['condition']
+        self.wind_direction = weather['wind']['direction']
+        self.wind_speed = weather['wind']['speed']['kmh']
+        self.temperature = weather['temp']['celsius']
+        self.arrivals = self.process_flights(schedule['arrivals']['data'])
+        self.departures = self.process_flights(schedule['departures']['data'])
+
+    def process_flights(self, flights):
+        df = pd.json_normalize(flights)
+        df['final_time'] = pd.to_datetime(df['flight.time.estimated.arrival'].fillna(df['flight.time.scheduled.arrival']) + df['flight.airport.destination.timezone.offset'], unit='s')
+        return df
+
     def get_arrivals(self):
         """Returns arrivals dataframe"""
-        arrivals_df = pd.json_normalize(self.arrivals)
-        arrivals_df['arrival_time'] = pd.to_datetime(arrivals_df['flight.time.scheduled.arrival'] + arrivals_df['flight.airport.destination.timezone.offset'], unit='s')
-        arrivals_df['arrival_estimated_time'] = pd.to_datetime(arrivals_df['flight.time.estimated.arrival'] + arrivals_df['flight.airport.destination.timezone.offset'], unit='s')
-        arrivals_df['final_time'] = arrivals_df['arrival_estimated_time'].fillna(arrivals_df['arrival_time'])
         columns_to_leave = ['flight.status.generic.status.type', 'flight.identification.number.default', 'flight.identification.callsign', 'flight.aircraft.model.text', 'flight.aircraft.country.name', 'flight.airline.short', 'flight.airport.origin.position.region.city', 'final_time']
-        return arrivals_df[columns_to_leave]
+        return self.arrivals[columns_to_leave]
 
     def get_departures(self):
         """Returns departures dataframe"""
-        departures_df = pd.json_normalize(self.departures)
-        departures_df['departure_time'] = pd.to_datetime(departures_df['flight.time.scheduled.departure'] + departures_df['flight.airport.origin.timezone.offset'], unit='s')
-        departures_df['departure_estimated_time'] = pd.to_datetime(departures_df['flight.time.estimated.departure'] + departures_df['flight.airport.origin.timezone.offset'], unit='s')
-        departures_df['final_time'] = departures_df['departure_estimated_time'].fillna(departures_df['departure_time'])
         columns_to_leave = ['flight.status.generic.status.type', 'flight.identification.number.default', 'flight.aircraft.model.text', 'flight.aircraft.country.name', 'flight.airline.short', 'flight.airport.destination.position.region.city', 'final_time']
-        return departures_df[columns_to_leave]
+        return self.departures[columns_to_leave]
     
     def concat_arrivals_departures(self):
         """Returns arrivals and departures dataframe concatenated"""
-        arrivals_df = self.get_arrivals()
-        departures_df = self.get_departures()
-        flights_df = pd.concat([arrivals_df, departures_df])
-        flights_df = flights_df.sort_values(by='final_time', ascending=True)
-        flights_df = flights_df.fillna('')
+        flights_df = pd.concat([self.get_arrivals(), self.get_departures()])
+        flights_df = flights_df.sort_values(by='final_time', ascending=True).fillna('')
         # Add Vilnius city to origin and destination
         flights_df.loc[flights_df['flight.status.generic.status.type'] == 'departure', 'flight.airport.origin.position.region.city'] = 'Vilnius'
         flights_df.loc[flights_df['flight.status.generic.status.type'] == 'arrival', 'flight.airport.destination.position.region.city'] = 'Vilnius'
