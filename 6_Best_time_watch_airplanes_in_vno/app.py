5f90abbba740af2c57a46591947c28c1b28744ed
Kodotautas <lukosiunas.v@gmail.com>
2023-11-17 07:29:56 +0200

    saving

--- 6_Best_time_watch_airplanes_in_vno/app.py
+++ 6_Best_time_watch_airplanes_in_vno/app.py
@@ -1,53 +1,3 @@
-import pandas as pd
-
-from flask import Flask, render_template
-from helpers.flights_data import FlightData
-import datetime
-
-app = Flask(__name__)
-
-flight_data = FlightData('EYVI')
-
-@app.route('/')
-def best_time():
-    # Get flights and weather data and filter by today and tomorrow
-    flights = flight_data.group_flights_by_final_time()
-    flights = flights[(flights['Datetime'].dt.date == pd.Timestamp.today().date()) | (flights['Datetime'].dt.date == pd.Timestamp.today().date() + pd.Timedelta(days=1))]
-    weather_data = flight_data.get_weather()
-
-    # Prepare data for Chart.js
-    labels = flights.sort_values('Datetime', ascending=True)['Datetime'].tolist()
-    labels = [datetime.datetime.strftime(x, '%Y-%m-%d %H:%M') for x in labels]
-    data = flights.sort_values('Datetime', ascending=True)['Flights count'].tolist()
-
-    # Pass data to template
-    return render_template('best_time.html', labels=labels, data=data, weather_data=weather_data)
-
-
-@app.route('/vno_flights_data')
-def flights_data():
-    # Get flights and weather data and filter by today and tomorrow
-    flights_df = flight_data.concat_arrivals_departures()
-    weather_data = flight_data.get_weather()
-
-    # rename columns
-    flights_df.rename(columns={'flight.status.generic.status.type': 'Type', 
-                               'flight.identification.number.default': 'Flight number',
-                               'flight.aircraft.model.text': 'Aircraft model', 
-                               'flight.aircraft.country.name': 'Aircraft country', 
-                               'flight.airline.short': 'Airline', 
-                               'flight.airport.origin.position.region.city': 'Origin city', 
-                               'flight.airport.destination.position.region.city': 'Destination city', 
-                               'final_time': 'Final time'}, inplace=True)
-
-    return render_template('flights_data.html', weather_data=weather_data, flights_data=flights_df, index=False)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
 # import pandas as pd
 
 # from flask import Flask, render_template
@@ -56,12 +6,14 @@ if __name__ == '__main__':
 
 # app = Flask(__name__)
 
+# flight_data = FlightData('EYVI')
+
 # @app.route('/')
 # def best_time():
 #     # Get flights and weather data and filter by today and tomorrow
-#     flights = FlightData('EYVI').group_flights_by_final_time()
+#     flights = flight_data.group_flights_by_final_time()
 #     flights = flights[(flights['Datetime'].dt.date == pd.Timestamp.today().date()) | (flights['Datetime'].dt.date == pd.Timestamp.today().date() + pd.Timedelta(days=1))]
-#     weather_data = FlightData('EYVI').get_weather()
+#     weather_data = flight_data.get_weather()
 
 #     # Prepare data for Chart.js
 #     labels = flights.sort_values('Datetime', ascending=True)['Datetime'].tolist()
@@ -75,22 +27,70 @@ if __name__ == '__main__':
 # @app.route('/vno_flights_data')
 # def flights_data():
 #     # Get flights and weather data and filter by today and tomorrow
-#     flights_df = FlightData('EYVI').concat_arrivals_departures()
-#     weather_data = FlightData('EYVI').get_weather()
+#     flights_df = flight_data.concat_arrivals_departures()
+#     weather_data = flight_data.get_weather()
 
 #     # rename columns
-#     flights_df = flights_df.rename(columns={'flight.status.generic.status.type': 'Type', 
-#                                             'flight.identification.number.default': 'Flight number',
-#                                             'flight.aircraft.model.text': 'Aircraft model', 
-#                                             'flight.aircraft.country.name': 'Aircraft country', 
-#                                             'flight.airline.short': 'Airline', 
-#                                             'flight.airport.origin.position.region.city': 'Origin city', 
-#                                             'flight.airport.destination.position.region.city': 'Destination city', 
-#                                             'final_time': 'Final time'})
+#     flights_df.rename(columns={'flight.status.generic.status.type': 'Type', 
+#                                'flight.identification.number.default': 'Flight number',
+#                                'flight.aircraft.model.text': 'Aircraft model', 
+#                                'flight.aircraft.country.name': 'Aircraft country', 
+#                                'flight.airline.short': 'Airline', 
+#                                'flight.airport.origin.position.region.city': 'Origin city', 
+#                                'flight.airport.destination.position.region.city': 'Destination city', 
+#                                'final_time': 'Final time'}, inplace=True)
 
-#     flights_data = flights_df.to_dict('records')
-
-#     return render_template('flights_data.html', weather_data=weather_data, flights_data=flights_data, index=False)
+#     return render_template('flights_data.html', weather_data=weather_data, flights_data=flights_df, index=False)
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
+
+
+
+
+import pandas as pd
+
+from flask import Flask, render_template
+from helpers.flights_data import FlightData
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def best_time():
+    # Get flights and weather data and filter by today and tomorrow
+    flights = FlightData('EYVI').group_flights_by_final_time()
+    flights = flights[(flights['Datetime'].dt.date == pd.Timestamp.today().date()) | (flights['Datetime'].dt.date == pd.Timestamp.today().date() + pd.Timedelta(days=1))]
+    weather_data = FlightData('EYVI').get_weather()
+
+    # Prepare data for Chart.js
+    labels = flights.sort_values('Datetime', ascending=True)['Datetime'].tolist()
+    labels = [datetime.datetime.strftime(x, '%Y-%m-%d %H:%M') for x in labels]
+    data = flights.sort_values('Datetime', ascending=True)['Flights count'].tolist()
+
+    # Pass data to template
+    return render_template('best_time.html', labels=labels, data=data, weather_data=weather_data)
+
+
+@app.route('/vno_flights_data')
+def flights_data():
+    # Get flights and weather data and filter by today and tomorrow
+    flights_df = FlightData('EYVI').concat_arrivals_departures()
+    weather_data = FlightData('EYVI').get_weather()
+
+    # rename columns
+    flights_df = flights_df.rename(columns={'flight.status.generic.status.type': 'Type', 
+                                            'flight.identification.number.default': 'Flight number',
+                                            'flight.aircraft.model.text': 'Aircraft model', 
+                                            'flight.aircraft.country.name': 'Aircraft country', 
+                                            'flight.airline.short': 'Airline', 
+                                            'flight.airport.origin.position.region.city': 'Origin city', 
+                                            'flight.airport.destination.position.region.city': 'Destination city', 
+                                            'final_time': 'Final time'})
+
+    flights_data = flights_df.to_dict('records')
+
+    return render_template('flights_data.html', weather_data=weather_data, flights_data=flights_data, index=False)
+
+if __name__ == '__main__':
+    app.run(debug=True)
