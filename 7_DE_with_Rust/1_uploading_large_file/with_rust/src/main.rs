db6be976e83e18cb49d6daf0c20f427015c5ac96
Kodotautas <lukosiunas.v@gmail.com>
2023-12-04 11:59:03 +0200

    saving compiled code

--- 7_DE_with_Rust/1_uploading_large_file/with_rust/src/main.rs
+++ 7_DE_with_Rust/1_uploading_large_file/with_rust/src/main.rs
@@ -1,72 +1,52 @@
-// filename: src/main.rs
-// fn main() {
-//     println!("Hello, world! Let's use data with Rust!");
-// }
-
-// use google_cloud_storage::client::{ClientConfig, Client};
-// // use google_cloud_storage::sign::SignedURLOptions;
-// // use google_cloud_storage::sign::SignBy;
-// // use google_cloud_storage::sign::SignedURLMethod;
-// use google_cloud_storage::http::Error;
-// // use google_cloud_storage::http::objects::download::Range;
-// // use google_cloud_storage::http::objects::get::GetObjectRequest;
-// use google_cloud_storage::http::objects::upload::{Media, UploadObjectRequest, UploadType};
-// // use tokio::task::JoinHandle;
-// // use std::fs::File;
-// // use std::io::BufReader;
-// // use std::io::Read;
-
-// async fn run(config: ClientConfig) -> Result<(), Error> {
-
-//     // Create client.
-//     let _config = ClientConfig::default().with_auth().await.unwrap();
-//     let client = Client::new(config);
-
-//     // Upload the file
-//     let upload_type = UploadType::Simple(Media::new("file.png"));
-//     let uploaded = client.upload_object(&UploadObjectRequest {
-//         bucket: "bucfiles-to-experiment".to_string(),
-//         ..Default::default()
-//     }, "hello world".as_bytes(), &upload_type).await;
-
-//     match uploaded {
-//         Ok(_) => println!("Connection successful, file uploaded."),
-//         Err(e) => println!("Failed to upload file: {}", e),
-//     }
-
-//     // Download the file
-// //     let data = client.download_object(&GetObjectRequest {
-// //         bucket: "bucket".to_string(),
-// //         object: "file.png".to_string(),
-// //         ..Default::default()
-// //    }, &Range::default()).await;
-
-//     // Create signed url with the default key and google-access-id of the client
-//     // let url_for_download = client.signed_url("bucket", "foo.txt", None, None, SignedURLOptions::default());
-//     // let url_for_upload = client.signed_url("bucket", "foo.txt", None, None, SignedURLOptions {
-//     //     method: SignedURLMethod::PUT,
-//     //     ..Default::default()
-//     // });
-
-//     Ok(())
-// }
-
-// fn main() {
-//     let config = ClientConfig::default();
-//     let rt = tokio::runtime::Runtime::new().unwrap();
-//     rt.block_on(run(config)).unwrap();
-// }
-
 use google_cloud_storage::client::{ClientConfig, Client};
+use google_cloud_storage::http::objects::upload::{Media, UploadObjectRequest, UploadType};
+use std::time::Instant;
+use std::fs::read;
+use reqwest::Body;
 
-async fn run() {
-    let config = ClientConfig::default().with_auth().await.unwrap();
-    let _client = Client::new(config);
+async fn run(config: ClientConfig) -> Result<(), Box<dyn std::error::Error>> {
 
+    // Create client.
+    let _config = ClientConfig::default().with_auth().await.unwrap();
+    let client = Client::new(config);
+
+    // Read the file into a byte array
+    let file_path = "/home/vytautas/Desktop/archive.zip";
+    let data = match read(file_path) {
+        Ok(data) => data,
+        Err(e) => {
+            println!("Failed to read file: {}", e);
+            return Err(Box::new(e));
+        }
+    };
+
+    // Convert Vec<u8> to Body
+    let body = Body::from(data);
+
+    // Upload the file
+    let upload_type = UploadType::Simple(Media::new(file_path));
+    let uploaded = client.upload_object(&UploadObjectRequest {
+        bucket: "files-to-experiment".to_string(),
+        ..Default::default()
+    }, body, &upload_type).await;
+
+    match uploaded {
+        Ok(_) => println!("Connection successful, file uploaded."),
+        Err(e) => println!("Failed to upload file: {}", e),
+    }
+
+    Ok(())
 }
 
-// run run function
-fn main() {
-    let rt = tokio::runtime::Runtime::new().unwrap();
-    rt.block_on(run());
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let start = Instant::now();
+
+    let config = ClientConfig::default().with_auth().await.unwrap();
+    run(config).await?;
+
+    let duration = start.elapsed();
+    println!("Upload completed in {} seconds", duration.as_secs_f64());
+
+    Ok(())
 }
