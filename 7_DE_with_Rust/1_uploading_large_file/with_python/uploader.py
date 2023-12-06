03ab0d1012125f408b31a3e7b7f028957c5d493d
Kodotautas <lukosiunas.v@gmail.com>
2023-12-06 04:07:24 +0200

    added rust zip function

--- 7_DE_with_Rust/1_uploading_large_file/with_python/uploader.py
+++ 7_DE_with_Rust/1_uploading_large_file/with_python/uploader.py
@@ -2,23 +2,36 @@ import os
 import time
 from google.cloud import storage
 
-def upload_file(project_name, bucket_name, source_file_name, destination_blob_name):
-    """Uploads a file to the Google Cloud Storage bucket."""
-    storage_client = storage.Client(project=project_name)
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
+class File:
+    def zip_file(source_file_name, destination_file_name):
+        """Zips a file."""
+        print(f"Zipping {source_file_name} to {destination_file_name}...")
+        os.system(f"zip {destination_file_name} {source_file_name}")
 
-    blob.upload_from_filename(source_file_name)
+    def upload_file(project_name, bucket_name, destination_file_name, destination_blob_name):
+        """Uploads a file to the Google Cloud Storage bucket."""
+        storage_client = storage.Client(project=project_name)
+        bucket = storage_client.bucket(bucket_name)
+        blob = bucket.blob(destination_blob_name)
+        
+        print(f"Uploading {destination_file_name} to {destination_blob_name}...")
+        blob.upload_from_filename(destination_file_name)
 
-    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+        print(f"File {destination_file_name} uploaded to {destination_blob_name}.")
 
 # Set variables
 project_name = "data-engineering-with-rust"
 bucket_name = "files-to-experiment"
-source_file_name = "/home/vytautas/Desktop/archive.zip"
+source_file_name = "/home/vytautas/Desktop/itineraries.csv"
+destination_file_name = "/home/vytautas/Desktop/archive.zip"
 destination_blob_name = source_file_name.split("/")[-1]
 
 start = time.time()
-upload_file(project_name, bucket_name, source_file_name, destination_blob_name)
+File.zip_file(source_file_name, destination_file_name)
 end = time.time()
-print(f"Time elapsed: {end - start} seconds to upload {source_file_name} to {destination_blob_name} which size is {os.path.getsize(source_file_name)} bytes.")
+print(f"Time elapsed: {(end - start) / 60} minutes to zip {source_file_name} to {destination_file_name} which size is {os.path.getsize(destination_file_name)} bytes.")
+
+start = time.time()
+File.upload_file(project_name, bucket_name, destination_file_name, destination_blob_name)
+end = time.time()
+print(f"Time elapsed: {(end - start) / 60} minutes to upload {destination_file_name} to {destination_blob_name} which size is {os.path.getsize(destination_file_name)} bytes.")
--- 7_DE_with_Rust/1_uploading_large_file/with_rust/src/main.rs
+++ 7_DE_with_Rust/1_uploading_large_file/with_rust/src/main.rs
@@ -3,16 +3,42 @@ use google_cloud_storage::http::objects::upload::{Media, UploadObjectRequest, Up
 use std::time::Instant;
 use std::fs::read;
 use reqwest::Body;
+use tokio::fs::File;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+use std::io::Write;
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use std::io::Cursor;
 
-async fn run(config: ClientConfig) -> Result<(), Box<dyn std::error::Error>> {
+async fn zip_file(file_path: String) -> io::Result<String> {
+    let mut buffer = [0; 1024];
+    let mut file = File::open(&file_path).await?;
+    let zip_file_path = format!("{}.gz", &file_path);
+    let mut zip_file = File::create(&zip_file_path).await?;
+    let mut encoder = GzEncoder::new(Cursor::new(Vec::new()), Compression::default());
+
+    while let Ok(n) = file.read(&mut buffer).await {
+        if n == 0 {
+            break;
+        }
+        encoder.write_all(&buffer[..n])?;
+    }
+
+    let inner = encoder.finish()?;
+    zip_file.write_all(inner.get_ref().as_slice()).await?;
+
+    Ok(zip_file_path)
+}
+
+
+async fn upload(config: ClientConfig, file_path: String) -> Result<(), Box<dyn std::error::Error>> {
 
     // Create client.
     let _config = ClientConfig::default().with_auth().await.unwrap();
     let client = Client::new(config);
 
     // Read the file into a byte array
-    let file_path = "/home/vytautas/Desktop/archive.zip";
-    let data = match read(file_path) {
+    let data = match read(&file_path) {
         Ok(data) => data,
         Err(e) => {
             println!("Failed to read file: {}", e);
@@ -24,7 +50,7 @@ async fn run(config: ClientConfig) -> Result<(), Box<dyn std::error::Error>> {
     let body = Body::from(data);
 
     // Upload the file
-    let upload_type = UploadType::Simple(Media::new(file_path));
+    let upload_type = UploadType::Simple(Media::new("archive.zip"));
     let uploaded = client.upload_object(&UploadObjectRequest {
         bucket: "files-to-experiment".to_string(),
         ..Default::default()
@@ -40,13 +66,21 @@ async fn run(config: ClientConfig) -> Result<(), Box<dyn std::error::Error>> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let start = Instant::now();
+    let start_zip = Instant::now();
+
+    let file_path = "/home/vytautas/Desktop/itineraries.csv".to_string();
+    let zip_file_path = zip_file(file_path).await?;
+
+    let duration_zip = start_zip.elapsed();
+    println!("File zipped in {} minutes", duration_zip.as_secs_f64() / 60.0);
+
+    let start_upload = Instant::now();
 
     let config = ClientConfig::default().with_auth().await.unwrap();
-    run(config).await?;
+    upload(config, zip_file_path).await?;
 
-    let duration = start.elapsed();
-    println!("Upload completed in {} seconds", duration.as_secs_f64());
+    let duration_upload = start_upload.elapsed();
+    println!("Upload completed in {} minutes", duration_upload.as_secs_f64() / 60.0);
 
     Ok(())
 }
