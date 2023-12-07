81a8feea22a9366caf198d7c117de1c1e5aba86e
Kodotautas <lukosiunas.v@gmail.com>
2023-12-07 08:16:45 +0200

    implemented rust polars

--- 7_DE_with_Rust/1_read_upload_csv/with_rust/src/main.rs
+++ 7_DE_with_Rust/1_read_upload_csv/with_rust/src/main.rs
@@ -1,19 +1,19 @@
+use std::fs;
 use std::fs::File;
 use std::io::Write;
 use std::time::Instant;
-use std::os::unix::fs::MetadataExt;
 use polars::prelude::*;
 
 struct FileHandler;
 
 impl FileHandler {
-    fn read_csv_with_polars(file_name: &str) -> Result<DataFrame> {
+    fn read_csv_with_polars(file_name: &str) -> Result<DataFrame, Box<dyn std::error::Error>> {
         let df = CsvReader::from_path(file_name)?.finish()?;
         Ok(df)
     }
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let source_file_name = "/home/vytautas/Desktop/alban_news.csv";
 
     let start = Instant::now();
@@ -23,7 +23,7 @@ fn main() -> Result<()> {
     let metadata = fs::metadata(source_file_name)?;
     let file_size = metadata.len();
 
-    let mut file = File::create("7_DE_with_Rust/1_read_upload_csv/with_rust/src/times.txt")?;
+    let mut file = File::create("times.txt")?;
     write!(file, "Time elapsed with Polars: {} seconds to read {} which size is {} bytes.\n", 
         duration.as_secs_f64(), source_file_name, file_size)?;
 
