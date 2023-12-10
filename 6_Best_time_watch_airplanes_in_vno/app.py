529422fa4b9847acdc2b89f3dccffc1a8c518e3d
Kodotautas <lukosiunas.v@gmail.com>
2023-12-10 17:57:42 +0200

    added more info in tooltip

--- 6_Best_time_watch_airplanes_in_vno/app.py
+++ 6_Best_time_watch_airplanes_in_vno/app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import json
 from flask import Flask, render_template
 from helpers.flights_data import FlightData
 import datetime
@@ -20,8 +20,27 @@ def best_time():
     labels = [datetime.datetime.strftime(x, '%Y-%m-%d %H:%M') for x in labels]
     data = flights.sort_values('Datetime', ascending=True)['Flights count'].tolist()
 
+    flights_df = flight_data.concat_arrivals_departures()
+    weather_data = flight_data.get_weather()
+
+    # rename columns
+    flights_df.rename(columns={'flight.status.generic.status.type': 'Type', 
+                               'flight.identification.number.default': 'Flight number',
+                               'flight.aircraft.model.text': 'Aircraft model', 
+                               'flight.aircraft.country.name': 'Aircraft country', 
+                               'flight.airline.short': 'Airline', 
+                               'flight.airport.origin.position.region.city': 'Origin city', 
+                               'flight.airport.destination.position.region.city': 'Destination city', 
+                               'final_time': 'Final time',
+                               'flight.status.generic.status.color': 'Status'}, inplace=True)
+
+    flights_df['Final time'] = flights_df['Final time'].dt.strftime('%Y-%m-%d %H:%M')
+
+    flights_data = flights_df.to_dict('records')
+    flights_data_json = json.dumps(flights_data)
+
     # Pass data to template
-    return render_template('best_time.html', labels=labels, data=data, weather_data=weather_data)
+    return render_template('best_time.html', labels=labels, data=data, weather_data=weather_data, flights_data=flights_data_json)
 
 
 @app.route('/vno_flights_data')
