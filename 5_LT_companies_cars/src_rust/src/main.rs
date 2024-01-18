1ec8043d80015e2aa1132621f8d8ed1cbecc3aae
Kodotautas <lukosiunas.v@gmail.com>
2024-01-18 08:13:18 +0200

    Refactor Downloader struct and add Google Cloud Storage authentication

--- 5_LT_companies_cars/src_rust/src/main.rs
+++ 5_LT_companies_cars/src_rust/src/main.rs
@@ -1,11 +1,12 @@
-use google_cloud_storage::client::Client;
-use google_cloud_storage::client::ClientConfig;
+use google_cloud_storage::client::{ClientConfig, Client};
 use google_cloud_storage::http::objects::upload::{Media, UploadObjectRequest, UploadType};
 struct Downloader;
 
+
 impl Downloader {
-    async fn run(config: ClientConfig, url: &str, bucket: &str, object_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    async fn run(_config: ClientConfig, url: &str, bucket: &str, object_name: &str) -> Result<(), Box<dyn std::error::Error>> {
         // Create client.
+        let config = ClientConfig::default().with_auth().await.unwrap();
         let client = Client::new(config);
 
         // Download the file
@@ -28,6 +29,8 @@ impl Downloader {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::env::set_var("GOOGLE_APPLICATION_CREDENTIALS", "/home/vytautas/.config/gcloud/application_default_credentials.json");
+    
     let config = ClientConfig::default();
     let url = "https://get.data.gov.lt/datasets/gov/ird/anr/KetPazeidejas/:format/csv";
     let bucket = "lithuania_statistics";
