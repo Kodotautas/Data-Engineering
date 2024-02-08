a6e41aab096eacfdee1062257e9bfd7ce009dd73
Kodotautas <lukosiunas.v@gmail.com>
2024-02-08 14:22:18 +0200

    Add Pipeline struct and processing method

--- 8_Real_time_streaming_Rust/streamer/src/main.rs
+++ 8_Real_time_streaming_Rust/streamer/src/main.rs
@@ -2,6 +2,7 @@ use futures::{SinkExt, StreamExt};
 use tokio_tungstenite::tungstenite::protocol::Message;
 use url::Url;
 use tokio::time::{Duration, sleep};
+struct Pipeline;
 
 #[tokio::main]
 async fn main() {
@@ -33,41 +34,42 @@ async fn main() {
     let _ = futures::join!(read, write);
 }
 
+impl Pipeline{
+    fn processing(message: &str) {
+        let data: serde_json::Value = serde_json::from_str(message).unwrap();
+        let info = match data["data"]["properties"].as_object() {
+            Some(info) => info,
+            None => {
+                println!("Error: 'properties' field not found");
+                return;
+            }
+        };
+        
+        let action = match data["action"].as_str() {
+            Some(action) => action,
+            None => {
+                println!("Error: 'action' field not found");
+                return;
+            }
+        };
 
-fn processing(message: &str) {
-    let data: serde_json::Value = serde_json::from_str(message).unwrap();
-    let info = match data["data"]["properties"].as_object() {
-        Some(info) => info,
-        None => {
-            println!("Error: 'properties' field not found");
-            return;
-        }
-    };
-    
-    let action = match data["action"].as_str() {
-        Some(action) => action,
-        None => {
-            println!("Error: 'action' field not found");
-            return;
-        }
-    };
+        println!(
+            ">>>> {action:7} event from {auth:7}, unid:{unid}, T0:{time}, Mag:{mag}, Region: {flynn_region}",
+            action = action,
+            auth = info["auth"].as_str().unwrap_or("unknown"),
+            unid = info["unid"].as_str().unwrap_or("unknown"),
+            time = info["time"].as_str().unwrap_or("unknown"),
+            mag = info["mag"].as_f64().unwrap_or(0.0),
+            flynn_region = info["flynn_region"].as_str().unwrap_or("unknown")
+        );
 
-    println!(
-        ">>>> {action:7} event from {auth:7}, unid:{unid}, T0:{time}, Mag:{mag}, Region: {flynn_region}",
-        action = action,
-        auth = info["auth"].as_str().unwrap_or("unknown"),
-        unid = info["unid"].as_str().unwrap_or("unknown"),
-        time = info["time"].as_str().unwrap_or("unknown"),
-        mag = info["mag"].as_f64().unwrap_or(0.0),
-        flynn_region = info["flynn_region"].as_str().unwrap_or("unknown")
-    );
+        // concat the message to a file and constantly append to it
+        let mut _file = std::fs::OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("streamer.txt")
+            .unwrap();
 
-    // concat the message to a file and constantly append to it
-    let mut _file = std::fs::OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open("streamer.txt")
-        .unwrap();
-
-    let _ = writeln!(_file, "{}", message);
+        let _ = writeln!(_file, "{}", message);
+    }
 }
