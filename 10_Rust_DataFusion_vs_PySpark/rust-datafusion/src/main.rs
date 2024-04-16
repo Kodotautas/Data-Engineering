f66cc70330c24b62bae50d6e32b3e705c2fb8b96
Kodotautas <lukosiunas.v@gmail.com>
2024-04-16 07:32:40 +0300

    Update Rust DataFusion file paths and improve data reading in main.rs

--- 10_Rust_DataFusion_vs_PySpark/rust-datafusion/src/main.rs
+++ 10_Rust_DataFusion_vs_PySpark/rust-datafusion/src/main.rs
@@ -4,14 +4,21 @@ use std::time::Instant;
 
 #[tokio::main]
 async fn main() -> datafusion::error::Result<()> {
-  let now = Instant::now();
-  let fusion = SessionContext::new();
-  let df = fusion.read_csv("data/*.csv", CsvReadOptions::new()).await?;
-  
-//   let df = df.aggregate(vec![col("member_casual")], vec![count(col("ride_id"))])?;
+    let now = Instant::now();
+    let fusion = SessionContext::new();
+    // get current working directory
+    let current_dir = std::env::current_dir()?;
+    
+    // Join with the 'data' directory
+    let data_dir = current_dir.join("data/*.csv");
+    println!("Reading data from: {:?}", data_dir);
 
-//   df.show_limit(100).await?;
-//   let elapsed = now.elapsed();
-//   println!("Elapsed: {:.2?}", elapsed);
-  Ok(())
+    let df = fusion.read_csv(data_dir.to_str().unwrap(), CsvReadOptions::new()).await?;
+  
+    let df = df.aggregate(vec![col("Time")], vec![count(col("Amount"))])?;
+
+    df.show_limit(100).await?;
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+    Ok(())
 }
