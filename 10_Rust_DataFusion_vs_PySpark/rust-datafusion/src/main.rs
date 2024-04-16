ba0979558d2d52e3afd9fe11c207a50a60ce0e54
Kodotautas <lukosiunas.v@gmail.com>
2024-04-16 07:54:58 +0300

    Refactor Rust DataFusion main.rs to filter and aggregate data

--- 10_Rust_DataFusion_vs_PySpark/rust-datafusion/src/main.rs
+++ 10_Rust_DataFusion_vs_PySpark/rust-datafusion/src/main.rs
@@ -13,11 +13,18 @@ async fn main() -> datafusion::error::Result<()> {
     let data_dir = current_dir.join("data/*.csv");
     println!("Reading data from: {:?}", data_dir);
 
+    // create a Dataframe
     let df = fusion.read_csv(data_dir.to_str().unwrap(), CsvReadOptions::new()).await?;
-  
-    let df = df.aggregate(vec![col("Time")], vec![count(col("Amount"))])?;
 
-    df.show_limit(100).await?;
+    // create a plan
+    let df = df.filter(col("Time").lt_eq(col("Amount")))?
+    .aggregate(vec![col("Time")], vec![sum(col("Amount"))])?
+    .limit(0, Some(100))?;
+
+    // execute the plan
+    let results = df.collect().await?;
+    println!("Results: {:?}", results);
+
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
     Ok(())
