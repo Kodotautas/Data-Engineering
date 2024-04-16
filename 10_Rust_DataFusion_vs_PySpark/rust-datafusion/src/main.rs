c8647a622a9ef4e1fab6b84ab1336cb6d39a4c9d
Kodotautas <lukosiunas.v@gmail.com>
2024-04-16 07:56:29 +0300

    made code shorter

--- 10_Rust_DataFusion_vs_PySpark/rust-datafusion/src/main.rs
+++ 10_Rust_DataFusion_vs_PySpark/rust-datafusion/src/main.rs
@@ -6,26 +6,14 @@ use std::time::Instant;
 async fn main() -> datafusion::error::Result<()> {
     let now = Instant::now();
     let fusion = SessionContext::new();
-    // get current working directory
-    let current_dir = std::env::current_dir()?;
-    
-    // Join with the 'data' directory
-    let data_dir = current_dir.join("data/*.csv");
-    println!("Reading data from: {:?}", data_dir);
+    let data_dir = std::env::current_dir()?.join("data/*.csv");
 
-    // create a Dataframe
-    let df = fusion.read_csv(data_dir.to_str().unwrap(), CsvReadOptions::new()).await?;
+    let df = fusion.read_csv(data_dir.to_str().unwrap(), CsvReadOptions::new()).await?
+        .filter(col("Time").lt_eq(col("Amount")))?
+        .aggregate(vec![col("Time")], vec![sum(col("Amount"))])?
+        .limit(0, Some(100))?;
 
-    // create a plan
-    let df = df.filter(col("Time").lt_eq(col("Amount")))?
-    .aggregate(vec![col("Time")], vec![sum(col("Amount"))])?
-    .limit(0, Some(100))?;
-
-    // execute the plan
-    let results = df.collect().await?;
-    println!("Results: {:?}", results);
-
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
+    println!("Results: {:?}", df.collect().await?);
+    println!("Elapsed: {:.2?}", now.elapsed());
     Ok(())
 }
